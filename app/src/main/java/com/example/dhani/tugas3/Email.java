3a68c6621cde3681be8efa463cf0e0ea399a1c81
dhani080296 <aramdhani14@gmail.com>
2017-12-10 06:03:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dhani/tugas3/Email.java
@@ -0,0 +1,23 @@
+package com.example.dhani.tugas3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Email extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email);
+    }
+    public void showText(View view) {
+        EditText editText= (EditText) findViewById(R.id.editText_main);
+        if(editText != null){
+            String showString = editText.getText().toString();
+            Toast.makeText(this, showString, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
